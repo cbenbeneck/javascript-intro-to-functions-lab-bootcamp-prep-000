d280eb02d2eddb66c57bfa2e6db8fac45f77ba59
Christina Benbeneck <cbenbeneck@mail.usf.edu>
2018-09-17 03:06:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ function sayHiToGrandma (string) {
   if (string.toLowerCase() === string) { 
     return "I can't hear you!"
     
-  if (string.toUpperCase() === string) {
+  else if (string.toUpperCase() === string) {
     return "YES INDEED!"
     
-    if ("I love you, Grandma." === string) {
+    else if ("I love you, Grandma." === string) {
       return "I love you, too."
     }
     
