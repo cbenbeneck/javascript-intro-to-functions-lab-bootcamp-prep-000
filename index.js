034885a1ed06fa115e3b6b717e2ffbf2f2977319
Christina Benbeneck <cbenbeneck@mail.usf.edu>
2018-09-17 03:01:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,5 +17,8 @@ function logWhisper (string) {
 }
 
 function sayHiToGrandma (string) {
-  if (string.toUpperCase() === string) 
+  if (string.tolowerCase() === string) { 
+    return, "I can't hear you!"
+    
+  if 
 }
