2f4d53388d6bf34dfab68de54c4bb6f64e884ab9
Christina Benbeneck <cbenbeneck@mail.usf.edu>
2018-09-17 03:19:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,20 +16,7 @@ function logWhisper (string) {
   console.log (string.toLowerCase())
 }
 
-function sayHiToGrandma(string) {
-  
-   if (string.toUpperCase() === string) {
-     
-    return "YES INDEED!"
-    
-  } else if (string === "I love you, Grandma.") {
-    
-    return "I love you, too." {
-      
-  } else {
-    
-    return ("I can't hear you!")
-  } 
-    
+
+
     
   
